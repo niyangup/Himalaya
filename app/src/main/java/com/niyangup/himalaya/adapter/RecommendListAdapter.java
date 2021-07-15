1c4fd5d29f8b6bd1f09423cf0b0b2f2ed083ad2e
poper <poper@wangxutech.com>
2021-07-15 13:00:33 +0800

    完成首页列表展示

--- app/src/main/java/com/niyangup/himalaya/adapter/RecommendListAdapter.java
+++ app/src/main/java/com/niyangup/himalaya/adapter/RecommendListAdapter.java
@@ -1,12 +1,16 @@
 package com.niyangup.himalaya.adapter;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.niyangup.himalaya.R;
+import com.squareup.picasso.Picasso;
 import com.ximalaya.ting.android.opensdk.model.album.Album;
 
 import java.util.ArrayList;
@@ -25,7 +29,7 @@ public class RecommendListAdapter extends RecyclerView.Adapter<RecommendListAdap
 
     @Override
     public void onBindViewHolder(@NonNull RecommendListAdapter.InnerHolder holder, int position) {
-
+        holder.setData(data.get(position));
     }
 
     @Override
@@ -44,6 +48,24 @@ public class RecommendListAdapter extends RecyclerView.Adapter<RecommendListAdap
     public static class InnerHolder extends RecyclerView.ViewHolder {
         public InnerHolder(@NonNull View itemView) {
             super(itemView);
+        }
+
+        public void setData(Album album) {
+            ImageView img = itemView.findViewById(R.id.album_cover);
+            TextView tvTitle = itemView.findViewById(R.id.album_title_tv);
+            TextView tvSubtitleTitle = itemView.findViewById(R.id.album_subtitle_tv);
+            TextView tvCount = itemView.findViewById(R.id.album_play_count);
+            TextView tvSize = itemView.findViewById(R.id.album_content_size);
+
+
+            tvTitle.setText(album.getAlbumTitle());
+            tvSubtitleTitle.setText(album.getAlbumIntro());
+            tvCount.setText(album.getPlayCount() + "");
+            tvSize.setText(album.getIncludeTrackCount() + "");
+
+            if (!TextUtils.isEmpty(album.getCoverUrlLarge())) {
+                Picasso.with(itemView.getContext()).load(album.getCoverUrlLarge()).into(img);
+            }
         }
     }
 }
